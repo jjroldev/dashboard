edfd12880f354bc8a33cdcc8bcf845bbed72b7ec
Justyn Roldan <justinjaelroldanmendez123@gmail.com>
2024-12-08 17:51:53 -0500

    Component IndicatorWeather

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 // Grid version 2
 import Grid from '@mui/material/Grid2' 
 import './App.css'
-
+import IndicatorWeather from './components/IndicatorWeather';
 function App() {
   return (
     <Grid container spacing={5}>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather /></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather /></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather /></Grid>
+        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather /></Grid>
 
         {/* Tabla */}
         <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
